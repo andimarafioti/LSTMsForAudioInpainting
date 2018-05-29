bccafc4f4fb46c63c6a7ba300260a3fdb7965511
andimarafioti <andimarafioti@gmail.com>
2018-05-29 17:49:37 +0200

    Added parameters for the context encoder

--- testArchitecture.py
+++ testArchitecture.py
@@ -2,19 +2,26 @@ from architecture.parameters.lstmParameters import LstmParameters
 from architecture.simpleLSTMArchitecture import SimpleLSTMArchitecture
 from system.lstmPreAndPostProcessor import LSTMPreAndPostProcessor
 from system.lstmSystem import LSTMSystem
-
+import tensorflow as tf
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-sessionsName = "test_newNewArch_"
+sessionsName = "test_mag_"
 
 batch_size = 64
 params = LstmParameters(lstmSize=512, batchSize=batch_size, signalLength=5120, fftWindowLength=128, fftHopSize=32,
 						countOfFrames=4)
 
-aContextEncoderArchitecture = SimpleLSTMArchitecture(inputShape=(params.batchSize(),
+with tf.variable_scope("forward_LSTM"):
+	aForwardLSTM = SimpleLSTMArchitecture(inputShape=(params.batchSize(),
 																 params.fftFrames()-1,
 																 params.fftFreqBins()), lstmParams=params)
+with tf.variable_scope("backward_LSTM"):
+	aBackwardLSTM = SimpleLSTMArchitecture(inputShape=(params.batchSize(),
+																 params.fftFrames()-1,
+																 params.fftFreqBins()), lstmParams=params)
+
+aPreProcessor = PreAndPostProcessor(params)
 
 aPreProcessor = LSTMPreAndPostProcessor(params)
 
