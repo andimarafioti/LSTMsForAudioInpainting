171e87a83ce696eb3c4963419312ea65aec4ef11
andimarafioti <andimarafioti@gmail.com>
2018-05-30 13:43:49 +0200

    Test the project

--- testArchitecture.py
+++ testArchitecture.py
@@ -1,30 +1,21 @@
-from architecture.parameters.lstmParameters import LstmParameters
-from architecture.simpleLSTMArchitecture import SimpleLSTMArchitecture
-from system.lstmPreAndPostProcessor import LSTMPreAndPostProcessor
+from architecture.contextEncoderLSTMArchitecture import ContextEncoderLSTMArchitecture
+from architecture.parameters.lstmContextInpaintingParameters import LstmContextInpaintingParameters
 from system.lstmSystem import LSTMSystem
-import tensorflow as tf
 import os
+
+from system.preAndPostProcessor import PreAndPostProcessor
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-sessionsName = "test_mag_"
+sessionsName = "test_contextLSTM_"
 
-batch_size = 64
-params = LstmParameters(lstmSize=512, batchSize=batch_size, signalLength=5120, fftWindowLength=128, fftHopSize=32,
-						countOfFrames=4)
+params = LstmContextInpaintingParameters(lstmSize=512, batchSize=64, signalLength=5120, gapLength=1024,
+										 fftWindowLength=512, fftHopSize=128)
 
-with tf.variable_scope("forward_LSTM"):
-	aForwardLSTM = SimpleLSTMArchitecture(inputShape=(params.batchSize(),
-																 params.fftFrames()-1,
-																 params.fftFreqBins()), lstmParams=params)
-with tf.variable_scope("backward_LSTM"):
-	aBackwardLSTM = SimpleLSTMArchitecture(inputShape=(params.batchSize(),
-																 params.fftFrames()-1,
-																 params.fftFreqBins()), lstmParams=params)
+contextArchitecture = ContextEncoderLSTMArchitecture(params)
 
 aPreProcessor = PreAndPostProcessor(params)
 
-aPreProcessor = LSTMPreAndPostProcessor(params)
-
-aContextEncoderSystem = LSTMSystem(aContextEncoderArchitecture, batch_size, aPreProcessor, params, sessionsName)
+aContextEncoderSystem = LSTMSystem(contextArchitecture, aPreProcessor, params, sessionsName)
 
 aContextEncoderSystem.train("../variationalAutoEncoder/fake_w5120_g1024_h512.tfrecords", "../variationalAutoEncoder/fake_w5120_g1024_h512.tfrecords", 1e-3)
