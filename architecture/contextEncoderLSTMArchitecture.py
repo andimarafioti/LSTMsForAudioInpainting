df20e09ed62d991c5cc15835ded5e602d24c10f4
andimarafioti <andimarafioti@gmail.com>
2018-05-30 13:42:46 +0200

    context architecture

---
+++ architecture/contextEncoderLSTMArchitecture.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+import numpy as np
+from architecture.architecture import Architecture
+
+
+class ContextEncoderLSTMArchitecture(Architecture):
+    def __init__(self, lstmParams):
+        with tf.variable_scope("LSTMArchitecture"):
+            self._lstmParams = lstmParams
+            self._inputShape = (lstmParams.batchSize(), lstmParams.contextStftFrameCount(), lstmParams.fftFreqBins(), 2)
+            super().__init__()
+
+    def inputShape(self):
+        return self._inputShape
+
+    def _lossGraph(self):
+        with tf.variable_scope("Loss"):
+            # normalize values !! divide by max input and multiply output
+
+            targetSquaredNorm = tf.reduce_sum(tf.square(self._target), axis=[1, 2])
+
+            error = self._target - self._output
+            error_per_example = tf.reduce_sum(tf.square(error), axis=[1, 2])
+
+            reconstruction_loss = 0.5 * tf.reduce_sum(error_per_example * (1 + 5 / (targetSquaredNorm + 1e-4)))
+            lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()]) * 1e-5
+            total_loss = tf.add_n([reconstruction_loss, lossL2])
+
+            total_loss_summary = tf.summary.scalar("total_loss", total_loss)
+            l2_loss_summary = tf.summary.scalar("lossL2", lossL2)
+            rec_loss_summary = tf.summary.scalar("reconstruction_loss", reconstruction_loss)
+            self._lossSummaries = tf.summary.merge([rec_loss_summary, l2_loss_summary, total_loss_summary])
+
+            return total_loss
+
+    def _lstmNetwork(self, data, initial_state, reuse, name):
+        with tf.variable_scope(name, reuse=reuse):
+            rnn_cell = tf.contrib.rnn.BasicLSTMCell(self._lstmParams.lstmSize())
+            dataset = tf.unstack(data, axis=-2)
+
+            outputs, states = tf.nn.static_rnn(rnn_cell, dataset, initial_state=initial_state, dtype=tf.float32)
+
+            out_output = np.empty([data.shape[0], 0, self._lstmParams.fftFreqBins()])
+            weights = self._weight_variable([self._lstmParams.lstmSize(), self._lstmParams.fftFreqBins()])
+            biases = self._bias_variable([self._lstmParams.fftFreqBins()])
+
+            for output in outputs:
+                mat_muled = tf.matmul(output, weights) + biases
+                output = tf.reshape(mat_muled, [-1, 1, self._lstmParams.fftFreqBins()])
+                out_output = tf.concat([out_output, output], axis=1)
+            return out_output, states
+
+    def _network(self, context, reuse=False):
+        with tf.variable_scope("Network", reuse=reuse):
+            #prepare data
+            forward_context = context[:, :, :, 0]
+            backward_context = tf.reverse(context[:, :, :, 1], axis=[1])
+
+            #run through network
+            forward_lstmed, forward_states = self._lstmNetwork(forward_context, None, reuse, 'forward_lstm')
+            backward_lstmed, backward_states = self._lstmNetwork(backward_context, None, reuse, 'backward_lstm')
+
+            forwards_gap = forward_lstmed[:, -1:, :]
+            backwards_gap = backward_lstmed[:, -1:, :]
+
+            for i in range(1, int(self._lstmParams.gapStftFrameCount())):
+                next_frame, forward_states = self._lstmNetwork(forwards_gap[:, -1:, :], forward_states, True, 'forward_lstm')
+                forwards_gap = tf.concat([forwards_gap, next_frame], axis=1)
+
+                previous_frame, backward_states = self._lstmNetwork(backwards_gap[:, -1:, :], backward_states, True, 'backward_lstm')
+                backwards_gap = tf.concat([backwards_gap, previous_frame], axis=1)
+
+            backwards_gap = tf.reverse(backwards_gap, axis=[1])
+
+            with tf.variable_scope('forward', reuse=reuse):
+                mixing_forward_variables = self._weight_variable([self._lstmParams.gapStftFrameCount()])
+            with tf.variable_scope('backward', reuse=reuse):
+                mixing_backward_variables = self._weight_variable([self._lstmParams.gapStftFrameCount()])
+
+            output = tf.zeros([self._lstmParams.batchSize(), 0, self._lstmParams.fftFreqBins()])
+
+            for i in range(int(self._lstmParams.gapStftFrameCount())):
+                intermediate_output = forwards_gap[:, i, :] * mixing_forward_variables[i] + backwards_gap[:, i, :] * mixing_backward_variables[i]
+                intermediate_output = tf.expand_dims(intermediate_output, axis=1)
+                output = tf.concat([output, intermediate_output], axis=1)
+
+            return output
+
+    def _weight_variable(self, shape):
+        return tf.get_variable('W', shape, initializer=tf.contrib.layers.xavier_initializer())
+
+    def _bias_variable(self, shape):
+        return tf.get_variable('bias', shape, initializer=tf.contrib.layers.xavier_initializer())
