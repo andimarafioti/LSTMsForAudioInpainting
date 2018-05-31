4e16b467e86dc64e7a7d519b098df5676c14672c
andimarafioti <andimarafioti@gmail.com>
2018-05-31 15:27:03 +0200

    fix for images!

--- system/realImagContextLSTMSystem.py
+++ system/realImagContextLSTMSystem.py
@@ -10,11 +10,21 @@ class RealImagContextLSTMSystem(LSTMSystem):
             self._pavlovs_SNR(self._architecture.output(), self._architecture.target(), onAxis=[1, 2, 3]))
 
     def _spectrogramImageSummary(self):
-        output = tf.abs(tf.transpose(self._architecture.output()[0]))
-        target = tf.abs(tf.transpose(self._architecture.target()[0]))
-        total = tf.abs(tf.transpose(tf.concat([self._architecture.input()[0, :, :, 0:2], self._architecture.output()[0],
-                          self._architecture.input()[0, :, :, 2:4]], axis=0)))
+        complexOutput = self._architecture.output()[0]
+        outputSpectrogram = tf.sqrt(tf.square(complexOutput[:, :, 0]) + tf.square(complexOutput[:, :, 1]))
 
-        return tf.summary.merge([tf.summary.image("Original", [colorize(target)]),
-                                tf.summary.image("Generated", [colorize(output)]),
-                                tf.summary.image("Complete", [colorize(total)])])
+        complexTarget = self._architecture.target()[0]
+        targetSpectrogram = tf.sqrt(tf.square(complexTarget[:, :, 0]) + tf.square(complexTarget[:, :, 1]))
+
+        complexLeft = self._architecture.input()[0, :, :, 0:2]
+        leftSpectrogram = tf.sqrt(tf.square(complexLeft[:, :, 0]) + tf.square(complexLeft[:, :, 1]))
+
+        complexRight = self._architecture.input()[0, :, :, 2:4]
+        rightSpectrogram = tf.sqrt(tf.square(complexRight[:, :, 0]) + tf.square(complexRight[:, :, 1]))
+
+        totalSpectrogram = tf.transpose(tf.concat([leftSpectrogram, outputSpectrogram,
+                                                   rightSpectrogram], axis=0))
+
+        return tf.summary.merge([tf.summary.image("Original", [colorize(tf.transpose(targetSpectrogram))]),
+                                tf.summary.image("Generated", [colorize(tf.transpose(outputSpectrogram))]),
+                                tf.summary.image("Complete", [colorize(totalSpectrogram)])])
