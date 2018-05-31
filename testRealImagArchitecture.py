4356f45aaafbfc2d9a1254f3de1b976a32853c5d
andimarafioti <andimarafioti@gmail.com>
2018-05-31 17:16:09 +0200

    test for cscs with several lstmCells

--- testRealImagArchitecture.py
+++ testRealImagArchitecture.py
@@ -7,7 +7,7 @@ from system.realImagContextLSTMSystem import RealImagContextLSTMSystem
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-sessionsName = "test_realImagContextLSTM_"
+sessionsName = "test_realImagContext3LSTM512_"
 
 params = LstmContextInpaintingParameters(lstmSize=512, batchSize=64, signalLength=5120, inputChannels=4,
 										 gapLength=1024, fftWindowLength=512, fftHopSize=128)
@@ -18,4 +18,4 @@ aPreProcessor = PreAndPostProcessor(params)
 
 aContextEncoderSystem = RealImagContextLSTMSystem(contextArchitecture, aPreProcessor, params, sessionsName)
 
-aContextEncoderSystem.train("../variationalAutoEncoder/fake_w5120_g1024_h512.tfrecords", "../variationalAutoEncoder/fake_w5120_g1024_h512.tfrecords", 1e-3)
+aContextEncoderSystem.train("../../aDataset/nsynth_train_w5120_g1024_h512.tfrecords", "../../aDataset/nsynth_valid_w5120_g1024_h512.tfrecords", 1e-3)
