498cacece3fb60ea5a65f02d607f41b21e6cc224
andimarafioti <andimarafioti@gmail.com>
2018-06-05 17:29:20 +0200

    the sizes don't match. Should think of a diff architecture to do that

--- architecture/realImagContextEncoderLSTMArchitecture.py
+++ architecture/realImagContextEncoderLSTMArchitecture.py
@@ -6,8 +6,6 @@ class RealImagContextEncoderLSTMArchitecture(ContextEncoderLSTMArchitecture):
     def _lossGraph(self):
         with tf.variable_scope("Loss"):
             reconstruction_loss = tf.reduce_sum(tf.square(self._target - self._output))
-            forward_reconstruction_loss = tf.reduce_sum(tf.square(self._target - self._forwardPrediction))
-            backward_reconstruction_loss = tf.reduce_sum(tf.square(self._target - self._backwardPrediction))
 
             lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()]) * 1e-5
             total_loss = tf.add_n([reconstruction_loss, lossL2])
@@ -15,10 +13,7 @@ class RealImagContextEncoderLSTMArchitecture(ContextEncoderLSTMArchitecture):
             total_loss_summary = tf.summary.scalar("total_loss", total_loss)
             l2_loss_summary = tf.summary.scalar("lossL2", lossL2)
             rec_loss_summary = tf.summary.scalar("reconstruction_loss", reconstruction_loss)
-            frec_loss_summary = tf.summary.scalar("forw_reconstruction_loss", forward_reconstruction_loss)
-            brec_loss_summary = tf.summary.scalar("back_reconstruction_loss", backward_reconstruction_loss)
-            self._lossSummaries = tf.summary.merge([rec_loss_summary, l2_loss_summary, total_loss_summary,
-                                                    frec_loss_summary, brec_loss_summary])
+            self._lossSummaries = tf.summary.merge([rec_loss_summary, l2_loss_summary, total_loss_summary])
 
             return total_loss
 
