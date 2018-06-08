a62efbcca853942292e46d9b22f4eaa9fbaa8b08
andimarafioti <andimarafioti@gmail.com>
2018-06-08 14:03:11 +0200

    Lazy fix

--- architecture/differenceContextEncoderLSTMArchitecture.py
+++ architecture/differenceContextEncoderLSTMArchitecture.py
@@ -1,22 +1,11 @@
 import tensorflow as tf
 import numpy as np
-from architecture.architecture import Architecture
+from architecture.contextEncoderLSTMArchitecture import ContextEncoderLSTMArchitecture
 
 
-class DifferenceContextEncoderLSTMArchitecture(Architecture):
-    def __init__(self, lstmParams):
-        with tf.variable_scope("LSTMArchitecture"):
-            self._lstmParams = lstmParams
-            self._inputShape = (lstmParams.batchSize(), lstmParams.contextStftFrameCount(), lstmParams.fftFreqBins(), lstmParams.inputChannels())
-            super().__init__()
-
-    def inputShape(self):
-        return self._inputShape
-
+class DifferenceContextEncoderLSTMArchitecture(ContextEncoderLSTMArchitecture):
     def _lossGraph(self):
         with tf.variable_scope("Loss"):
-            # normalize values !! divide by max input and multiply output
-
             forward_reconstruction_loss = tf.reduce_sum(tf.square(self._target - self._forwardPrediction))
             backward_reconstruction_loss = tf.reduce_sum(tf.square(self._target - self._backwardPrediction))
 
@@ -35,27 +24,21 @@ class DifferenceContextEncoderLSTMArchitecture(Architecture):
 
             return total_loss
 
-    def _lstmNetwork(self, data, initial_state, reuse, name):
-        with tf.variable_scope(name, reuse=reuse):
-            dataset = tf.unstack(data, axis=-2)
-
-            rnn_cell = tf.contrib.rnn.MultiRNNCell(
-                [tf.contrib.rnn.LSTMCell(self._lstmParams.lstmSize()),
-                 tf.contrib.rnn.LSTMCell(self._lstmParams.lstmSize()),
-                 tf.contrib.rnn.LSTMCell(self._lstmParams.lstmSize())])
-            outputs, states = tf.nn.static_rnn(rnn_cell, dataset, initial_state=initial_state, dtype=tf.float32)
-
-            out_output = np.empty([data.shape[0], 0, self._lstmParams.fftFreqBins()])
-            weights = self._weight_variable([self._lstmParams.lstmSize(), self._lstmParams.fftFreqBins()])
-            biases = self._bias_variable([self._lstmParams.fftFreqBins()])
-
-            for output in outputs:
-                mat_muled = tf.matmul(output, weights) + biases
-                output = tf.reshape(mat_muled, [-1, 1, self._lstmParams.fftFreqBins()])
-                out_output = tf.concat([out_output, output], axis=1)
-            return out_output, states
-
     def _network(self, context, reuse=False):
+        real_context = tf.stack([context[:, :, :, 0], context[:, :, :, 2]], axis=-1)
+        imag_context = tf.stack([context[:, :, :, 1], context[:, :, :, 3]], axis=-1)
+        real = self.__network(real_context, reuse)
+        real_forward = self._forwardPrediction
+        real_backward = self._backwardPrediction
+        imag = self.__network(imag_context, True)
+        imag_forward = self._forwardPrediction
+        imag_backward = self._backwardPrediction
+        self._forwardPrediction = tf.stack([real_forward, imag_forward], axis=-1)
+        self._backwardPrediction = tf.stack([real_backward, imag_backward], axis=-1)
+
+        return tf.stack([real, imag], axis=-1)
+
+    def __network(self, context, reuse=False):
         with tf.variable_scope("Network", reuse=reuse):
             #prepare data
             forward_context = context[:, :, :, 0]
@@ -104,9 +87,3 @@ class DifferenceContextEncoderLSTMArchitecture(Architecture):
                 intermediate_output = tf.expand_dims(intermediate_output, axis=1)
                 output = tf.concat([output, intermediate_output], axis=1)
             return output[:, 1:]
-
-    def _weight_variable(self, shape):
-        return tf.get_variable('W', shape, initializer=tf.contrib.layers.xavier_initializer())
-
-    def _bias_variable(self, shape):
-        return tf.get_variable('bias', shape, initializer=tf.contrib.layers.xavier_initializer())
