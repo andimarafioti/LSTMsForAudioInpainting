16419f517ac3df8ffe67caa35c6fb07dcee00800
andimarafioti <andimarafioti@gmail.com>
2018-06-08 14:24:07 +0200

    Difference is being tested with it's own net

--- testDiffRealImagArchitecture.py
+++ testDiffRealImagArchitecture.py
@@ -7,7 +7,7 @@ from system.realImagContextLSTMSystem import RealImagContextLSTMSystem
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-sessionsName = "differenceEncoder_"
+sessionsName = "batchSize256_"
 
 params = LstmContextInpaintingParameters(lstmSize=512, batchSize=256, signalLength=5120, inputChannels=4,
 										 gapLength=1024, fftWindowLength=512, fftHopSize=128)
@@ -18,4 +18,4 @@ aPreProcessor = PreAndPostProcessor(params)
 
 aContextEncoderSystem = RealImagContextLSTMSystem(contextArchitecture, aPreProcessor, params, sessionsName)
 
-aContextEncoderSystem.train("../../aDataset/nsynth_train_w5120_g1024_h512.tfrecords", "../../aDataset/nsynth_valid_w5120_g1024_h512.tfrecords", 1e-3)
+aContextEncoderSystem.train("../../aDataset/nsynth_train_w5120_g1024_h512.tfrecords", "../../aDataset/nsynth_valid_w5120_g1024_h512.tfrecords", 1e-4)
