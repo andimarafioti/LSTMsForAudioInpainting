af1b4ffabdc68d116c9de9731a6f8d951a6b41fe
andimarafioti <andimarafioti@gmail.com>
2018-06-09 10:19:10 +0200

    Continue training b256 fft 256

--- testRealImagArchitecture.py
+++ testRealImagArchitecture.py
@@ -7,10 +7,10 @@ from system.realImagContextLSTMSystem import RealImagContextLSTMSystem
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-sessionsName = "batchSize256_"
+sessionsName = "test_b256_fft256_"
 
 params = LstmContextInpaintingParameters(lstmSize=512, batchSize=256, signalLength=5120, inputChannels=4,
-										 gapLength=1024, fftWindowLength=512, fftHopSize=128)
+										 gapLength=1024, fftWindowLength=256, fftHopSize=64)
 
 contextArchitecture = RealImagContextEncoderLSTMArchitecture(params)
 
@@ -18,4 +18,4 @@ aPreProcessor = PreAndPostProcessor(params)
 
 aContextEncoderSystem = RealImagContextLSTMSystem(contextArchitecture, aPreProcessor, params, sessionsName)
 
-aContextEncoderSystem.train("../../aDataset/nsynth_train_w5120_g1024_h512.tfrecords", "../../aDataset/nsynth_valid_w5120_g1024_h512.tfrecords", 1e-4)
+aContextEncoderSystem.train("../../aDataset/nsynth_train_w5120_g1024_h512.tfrecords", "../../aDataset/nsynth_valid_w5120_g1024_h512.tfrecords", 1e-3)
