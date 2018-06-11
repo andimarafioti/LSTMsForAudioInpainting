b1650d48c626d8f8d14ce8680f102f4bd9dfc15a
andimarafioti <andimarafioti@gmail.com>
2018-06-11 16:32:40 +0200

    Test log shifted to one

--- system/logPreAndPostProcessor.py
+++ system/logPreAndPostProcessor.py
@@ -11,7 +11,7 @@ __author__ = 'Andres'
 class LogPreAndPostProcessor(PreAndPostProcessor):
     def _divideComplexIntoRealAndImag(self, complexTensor):
         magnitude = tf.abs(complexTensor)
-        shiftedMagnitude = tf.nn.relu(magnitude - 1e-4) + 1e-4
+        shiftedMagnitude = tf.nn.relu(magnitude - 1e-4) + 1
         logMagnitude = tf.log(shiftedMagnitude) / tf.log(tf.constant(10, dtype=shiftedMagnitude.dtype))
         angle = tf.angle(complexTensor)
         real_part = logMagnitude * tf.cos(angle)
--- testLogRealImagArchitecture.py
+++ testLogRealImagArchitecture.py
@@ -7,7 +7,7 @@ from system.realImagContextLSTMSystem import RealImagContextLSTMSystem
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-sessionsName = "test_logMagnitude_"
+sessionsName = "test_logMagnitude_shiftedToOne_"
 
 params = LstmContextInpaintingParameters(lstmSize=512, batchSize=256, signalLength=5120, inputChannels=4,
 										 gapLength=1024, fftWindowLength=512, fftHopSize=128)
