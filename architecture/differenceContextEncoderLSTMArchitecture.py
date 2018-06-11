37481c404db2353cbf4e90307e714ef718fc63dd
andimarafioti <andimarafioti@gmail.com>
2018-06-11 13:18:34 +0200

    Channel for real and imag

--- architecture/differenceContextEncoderLSTMArchitecture.py
+++ architecture/differenceContextEncoderLSTMArchitecture.py
@@ -25,3 +25,17 @@ class DifferenceContextEncoderLSTMArchitecture(ContextEncoderLSTMArchitecture):
                 output = tf.expand_dims(mat_muled, axis=1)
                 out_output = tf.concat([out_output, output], axis=1)
             return out_output, states
+
+    def _network(self, context, reuse=False):
+        real_context = tf.stack([context[:, :, :, 0], context[:, :, :, 2]], axis=-1)
+        imag_context = tf.stack([context[:, :, :, 1], context[:, :, :, 3]], axis=-1)
+        real = super()._network(real_context, reuse)
+        real_forward = self._forwardPrediction
+        real_backward = self._backwardPrediction
+        imag = super()._network(imag_context, True)
+        imag_forward = self._forwardPrediction
+        imag_backward = self._backwardPrediction
+        self._forwardPrediction = tf.stack([real_forward, imag_forward], axis=-1)
+        self._backwardPrediction = tf.stack([real_backward, imag_backward], axis=-1)
+
+        return tf.stack([real, imag], axis=-1)
