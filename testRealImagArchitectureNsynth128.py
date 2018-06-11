59c7a60b560b3059551745e2519f5c6b47ab27e9
andimarafioti <andimarafioti@gmail.com>
2018-06-11 16:04:18 +0200

    Restore nsynth 128ms fft256

--- testRealImagArchitectureNsynth128.py
+++ testRealImagArchitectureNsynth128.py
@@ -7,9 +7,9 @@ from system.realImagContextLSTMSystem import RealImagContextLSTMSystem
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-sessionsName = "test_realImagContext3LSTM512_fft256_nsynth2048_"
+sessionsName = "test_nsynth2048_bs256_"
 
-params = LstmContextInpaintingParameters(lstmSize=512, batchSize=64, signalLength=6144, inputChannels=4,
+params = LstmContextInpaintingParameters(lstmSize=512, batchSize=256, signalLength=6144, inputChannels=4,
 										 gapLength=2048, fftWindowLength=256, fftHopSize=64)
 
 contextArchitecture = RealImagContextEncoderLSTMArchitecture(params)
@@ -18,4 +18,4 @@ aPreProcessor = PreAndPostProcessor(params)
 
 aContextEncoderSystem = RealImagContextLSTMSystem(contextArchitecture, aPreProcessor, params, sessionsName)
 
-aContextEncoderSystem.train("../../aDataset/nsynth_train_w6144_g2048_h512.tfrecords", "../../aDataset/nsynth_valid_w6144_g2048_h512.tfrecords", 1e-3)
+aContextEncoderSystem.train("../../aDataset/nsynth_train_w6144_g2048_h512.tfrecords", "../../aDataset/nsynth_valid_w6144_g2048_h512.tfrecords", 1e-4, restoreNum=140000)
