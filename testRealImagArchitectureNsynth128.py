8bf07cd1ee14a0c7305b8a9d4069a3c308e7642f
andimarafioti <andimarafioti@gmail.com>
2018-06-11 16:13:53 +0200

    Restore gap 128ms fft 512

--- testRealImagArchitectureNsynth128.py
+++ testRealImagArchitectureNsynth128.py
@@ -7,10 +7,10 @@ from system.realImagContextLSTMSystem import RealImagContextLSTMSystem
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-sessionsName = "test_nsynth2048_bs256_"
+sessionsName = "test_nsynth2048_bs256_fft512_"
 
 params = LstmContextInpaintingParameters(lstmSize=512, batchSize=256, signalLength=6144, inputChannels=4,
-										 gapLength=2048, fftWindowLength=256, fftHopSize=64)
+										 gapLength=2048, fftWindowLength=512, fftHopSize=128)
 
 contextArchitecture = RealImagContextEncoderLSTMArchitecture(params)
 
@@ -18,4 +18,4 @@ aPreProcessor = PreAndPostProcessor(params)
 
 aContextEncoderSystem = RealImagContextLSTMSystem(contextArchitecture, aPreProcessor, params, sessionsName)
 
-aContextEncoderSystem.train("../../aDataset/nsynth_train_w6144_g2048_h512.tfrecords", "../../aDataset/nsynth_valid_w6144_g2048_h512.tfrecords", 1e-4, restoreNum=140000)
+aContextEncoderSystem.train("../../aDataset/nsynth_train_w6144_g2048_h512.tfrecords", "../../aDataset/nsynth_valid_w6144_g2048_h512.tfrecords", 1e-4)
