9b835217b12d27a3d1e133efe7b6498e547237a2
andimarafioti <andimarafioti@gmail.com>
2018-06-12 15:54:17 +0200

    Test logmagnitude shifted correctly

--- system/logPreAndPostProcessor.py
+++ system/logPreAndPostProcessor.py
@@ -1,7 +1,4 @@
-import functools
-
 import tensorflow as tf
-from tensorflow.contrib.signal.python.ops import window_ops
 
 from system.preAndPostProcessor import PreAndPostProcessor
 
@@ -11,9 +8,32 @@ __author__ = 'Andres'
 class LogPreAndPostProcessor(PreAndPostProcessor):
     def _divideComplexIntoRealAndImag(self, complexTensor):
         magnitude = tf.abs(complexTensor)
-        shiftedMagnitude = tf.nn.relu(magnitude - 1e-4) + 1
-        logMagnitude = tf.log(shiftedMagnitude) / tf.log(tf.constant(10, dtype=shiftedMagnitude.dtype))
+        croppedMagnitude = tf.maximum(magnitude, 1e-4)
+        logMagnitude = tf.log(croppedMagnitude) / tf.log(tf.constant(10, dtype=croppedMagnitude.dtype))
+        maxLogMag = 2
+        minLogMag = -4
+        outputMagnitude = (logMagnitude - minLogMag) / (maxLogMag - minLogMag)
+
         angle = tf.angle(complexTensor)
-        real_part = logMagnitude * tf.cos(angle)
-        imag_part = logMagnitude * tf.sin(angle)
+        real_part = outputMagnitude * tf.cos(angle)
+        imag_part = outputMagnitude * tf.sin(angle)
+        return tf.stack([real_part, imag_part], axis=-1, name='divideComplexIntoRealAndImag')
+
+    def _inversePrediction(self, tensor):
+        x = tensor[:, :, :, 0]
+        y = tensor[:, :, :, 1]
+
+        shifted_logmagnitude = tf.sqrt(tf.square(x) + tf.square(y))
+
+        angle = tf.sign(tf.asin(y / (shifted_logmagnitude))) * tf.acos(x / (shifted_logmagnitude))
+
+        maxLogMag = 2
+        minLogMag = -4
+
+        recenteredMag = (shifted_logmagnitude * (maxLogMag - minLogMag)) + minLogMag
+        magnitude = (10 ** recenteredMag)
+
+        real_part = magnitude * tf.cos(angle)
+        imag_part = magnitude * tf.sin(angle)
+
         return tf.stack([real_part, imag_part], axis=-1, name='divideComplexIntoRealAndImag')
