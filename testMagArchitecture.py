67f968c061d37e0da6f7dccf49f81618f99d990b
amarafioti <andimarafioti@gmail.com>
2018-08-03 18:05:21 +0200

    Test mag architecture

---
+++ testMagArchitecture.py
@@ -0,0 +1,24 @@
+from architecture.parameters.lstmContextInpaintingParameters import LstmContextInpaintingParameters
+from architecture.realImagContextEncoderLSTMArchitecture import RealImagContextEncoderLSTMArchitecture
+import os
+
+from system.magPreAndPostProcessor import MagPreAndPostProcessor
+from system.realImagContextLSTMSystem import RealImagContextLSTMSystem
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+sessionsName = "test_magnet_"
+
+params = LstmContextInpaintingParameters(lstmSize=512, batchSize=256, signalLength=5120, inputChannels=4,
+										 gapLength=1024, fftWindowLength=256, fftHopSize=64)
+
+contextArchitecture = RealImagContextEncoderLSTMArchitecture(params)
+
+aPreProcessor = MagPreAndPostProcessor(params)
+
+aContextEncoderSystem = RealImagContextLSTMSystem(contextArchitecture, aPreProcessor, params, sessionsName)
+
+# aContextEncoderSystem.train("../variationalAutoEncoder/nsynth_train_w5120_g1024_h512.tfrecords",
+# 							"../variationalAutoEncoder/nsynth_valid_w5120_g1024_h512.tfrecords", 1e-3)
+#
+aContextEncoderSystem.train("../../aDataset/nsynth_train_w5120_g1024_h512.tfrecords", "../../aDataset/nsynth_valid_w5120_g1024_h512.tfrecords", 1e-3)
